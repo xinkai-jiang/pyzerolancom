7b3925347b0cd7207142520d26fdaa03ab8bdb7d
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-12-07 16:19:19 +0100

    update service client

--- pyzerolancom/sockets/service_client.py
+++ pyzerolancom/sockets/service_client.py
@@ -1,22 +1,34 @@
+"""Client proxy for calling services."""
+from typing import Optional, cast, Dict
+import msgpack
+
+from ..utils.log import logger
+from ..nodes.lancom_node import LanComNode
+from ..utils.msg import send_bytes_request
+
 class ServiceProxy:
+    """Client proxy for calling services."""
+
     @staticmethod
     def request(
         service_name: str,
-        request_encoder: Callable[[RequestT], bytes],
-        response_decoder: Callable[[bytes], ResponseT],
-        request: RequestT,
-    ) -> Optional[ResponseT]:
+        request: Dict,
+    ) -> Optional[Dict]:
+        """Send a request to a service and get the response."""
         if LanComNode.instance is None:
             raise ValueError("Lancom Node is not initialized")
         node = LanComNode.instance
-        service_component = node.nodes_map.get_service_info(service_name)
+        service_component = node.nodes_manager.get_service_info(service_name)
         if service_component is None:
-            logger.warning(f"Service {service_name} is not exist")
+            logger.warning("Service %s is not exist", service_name)
             return None
-        request_bytes = request_encoder(request)
         addr = f"tcp://{service_component['ip']}:{service_component['port']}"
+        request_bytes = msgpack.packb(request)
+        if request_bytes is None:
+            logger.error("Failed to pack request for service %s", service_name)
+            return None
         response = node.loop_manager.submit_loop_task(
             send_bytes_request(addr, service_name, request_bytes),
             True,
         )
-        return response_decoder(cast(bytes, response))
+        return msgpack.unpackb(cast(bytes, response))
